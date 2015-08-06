cf511399fdeec382fa1cb7b8c93099bb1037c743
Nina <nbalach1@binghamton.edu>
2015-08-06 16:12:31 -0700

    second commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,14 @@
 (function(){
    angular.module('ticketApp')
        .controller('ticketAppController', function($scope, ticketReadFactory){
-           $scope.areas =[{name:'Sunnyvale'},{name:'Santa Clara'},{name:'Helsinki'},{name:'Toronto'}];
-           ticketReadFactory.readData().then(function(data){
-               console.log(data);
-           }, function(error){
-               console.log('could not read');
-           });
+           $scope.categories =[{name:'Location'},{name:'Status'},{name:'User'}];
+           $scope.result=[];
+           /*Click function called upon ng-click*/
+            $scope.getData = function(category){
+               //$scope.$apply
+                $scope.result=[];
+                ticketReadFactory.processData(category);
+                $scope.result = ticketReadFactory.result;
+            }
        });
 }());
--- js/services.js
+++ js/services.js
@@ -1,12 +1,60 @@
+/*Services defined here*/
+
 (function(){
    angular.module('ticketApp')
        .factory('ticketReadFactory', function($http){
+
            var service = {};
+           service.result = [];
+           /*read the .json fine and return a promise*/
            service.readData = function(){
                return $http.get('data/data.json');
            };
+
+           /*process data using the category specified*/
+
+           service.processData = function(category){
+               /*A promise*/
+               this.readData().then(function(data){
+                   service.result = process(category.name,data); //returns the object
+               }, function(error){
+                  return error;
+               });
+           };
            return service;
        });
+
+    function process(category, data){
+        var frequency ={};
+        Array.prototype.slice.call(data.data).forEach(function(item){
+            if(frequency[item[category]]){
+                frequency[item[category]]++;
+            }
+            else {
+                frequency[item[category]] = 1;
+            }
+        });
+        return frequency;
+    }
+
+
+       /*.factory('processFactory',function(){
+           var service ={};
+           service.frequency = {};
+           service.process = function(category, data){
+               Array.prototype.slice.call(data).forEach(function(item){
+                   console.log('here');
+                   console.log(category, item[category]);
+                   if(service.frequency[item[category]]){
+                            service.frequency[item[category]]++
+                   }
+                   else service.frequency[category] = 1;
+
+                });
+              // return frequency;
+           };
+           return service;
+       });*/
        /*.factory('ticketDataFactory', function(){
            var service = {};
            service.setData = function(){
